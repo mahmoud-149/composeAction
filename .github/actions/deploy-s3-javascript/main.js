1e06a5d8e724e4dc29fd8ab533b3d85f447e3a1d
mahmoud-149 <mahmoud14ahmedd@gmail.com>
2026-07-31 16:21:59 +0300

    try upload to aws and npm and bucker name3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,8 +3,8 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 function start(){
   const bucket = core.getInput('bucket',{required: true})
-  const bucket_region = core.getInput("bucket-region", {required: true});
-  const distFolder = core.getInput("dist-folder", { required: true });
+  const bucket_region = core.getInput('bucket_region', {required: true});
+  const distFolder = core.getInput('dist-folder', { required: true });
   
   const s3Uri= `s3://${bucket}`
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucket_region}`)

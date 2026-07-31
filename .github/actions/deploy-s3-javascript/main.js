d9b30c0a434e19c816b68e71ea193cb01ea5201f
mahmoud-149 <mahmoud14ahmedd@gmail.com>
2026-07-31 16:07:57 +0300

    try upload to aws

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,8 +2,15 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 const exec = require('@actions/exec')
 function start(){
-  core.notice('hello from my custom javascript action!')
-
+  const bucket = core.getInput('bucket',{required: true})
+  const bucket_region = core.getInput("bucker-region", {required: true});
+  const distFolder = core.getInput("dist-folder", { required: true });
+  
+  const s3Uri= `s3://${bucket}`
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucket_region}`)
+   
+  const websiteUrl = `http://${bucket}.s3-website-${bucket_region}.amazonaws.com`
+  core.setOutput('website-url',websiteUrl)
 }
 
 
